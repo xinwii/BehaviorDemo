d690a82abc10df3f9abbd74ca5497dc240b84642
xuxinwei <me67554@gmail.com>
2018-06-20 10:48:41 +0800

    demo run success

--- app/src/main/java/com/behaviordemo/app/MainActivity.kt
+++ app/src/main/java/com/behaviordemo/app/MainActivity.kt
@@ -2,16 +2,17 @@ package com.behaviordemo.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewCompat
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
+    var size=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun getNestedScrollView(v: View){
-
+        ViewCompat.offsetTopAndBottom(v,5)
     }
 }
